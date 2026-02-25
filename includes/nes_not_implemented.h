7d29b3715b6d6873b32159aef46a6afaa328a09c
Krishna Shetty <KrishnaHarishShetty@gmail.com>
2026-02-25 15:44:45 -0800

    Resolve constructor ambiguity in NotImplemented by simplifying overloads

--- includes/nes_not_implemented.h
+++ includes/nes_not_implemented.h
@@ -6,23 +6,19 @@
 
 namespace NES
 {
-    class NotImplemented : public std::logic_error
+    class NotImplemented final : public std::logic_error
     {
     private:
         std::string _text;
 
+    public:
         NotImplemented(const char* message, const char* functionName)
             : std::logic_error("Not Implemented")
             , _text(std::string(message) + " : " + (functionName ? functionName : ""))
         {}
 
-    public:
         explicit NotImplemented(const char* functionName)
             : NotImplemented("Not Implemented", functionName)
-        {}
-
-        NotImplemented(const char* message, const char* functionName)
-            : NotImplemented(message, functionName)
         {}
 
         const char* what() const noexcept override
