3239ac85a58d9c5f3ffd1d6c771f830676d7c64d
Krishna Shetty <KrishnaHarishShetty@gmail.com>
2026-05-30 22:58:23 -0700

    Implement PPU registers, bus communication, and vblank/NMI signaling

--- includes/nes_ppu.h
+++ includes/nes_ppu.h
@@ -2,6 +2,7 @@
 #define NES_PPU_H
 #include <SDL3/SDL.h>
 #include <functional>
+#include "nes_cartridge.h"
 
 namespace NES
 {
@@ -19,33 +20,150 @@ namespace NES
         SDL_Window *getWindow() const { return _window; }
         SDL_Renderer *getRenderer() const { return _renderer; }
 
-        struct Color
-        {
-            uint8_t r, g, b, a;
-        };
-
         bool isRunning() const { return _running; }
-        void handleEvents(std::function<void(SDL_Event&)> callback = nullptr);
+        void handleEvents(std::function<void(SDL_Event &)> callback = nullptr);
         void clear();
         void present();
         void clock();
 
-        void setClearColor(Color color);
+        void setClearColor(SDL_Color color);
+        uint8_t ppuRead(uint16_t address);
+        void ppuWrite(uint16_t address, uint8_t value);
+        uint8_t cpuRead(uint16_t address);
+        void cpuWrite(uint16_t address, uint8_t value);
+
+        static inline constexpr SDL_Color PALETTE[64] =
+            {
+                {84, 84, 84, 255},
+                {0, 30, 116, 255},
+                {8, 16, 144, 255},
+                {48, 0, 136, 255},
+                {68, 0, 100, 255},
+                {92, 0, 48, 255},
+                {84, 4, 0, 255},
+                {60, 24, 0, 255},
+                {32, 42, 0, 255},
+                {8, 58, 0, 255},
+                {0, 64, 0, 255},
+                {0, 60, 0, 255},
+                {0, 50, 60, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+                {152, 150, 152, 255},
+                {8, 76, 196, 255},
+                {48, 50, 236, 255},
+                {92, 30, 228, 255},
+                {136, 20, 176, 255},
+                {160, 20, 100, 255},
+                {152, 34, 32, 255},
+                {120, 60, 0, 255},
+                {84, 90, 0, 255},
+                {40, 114, 0, 255},
+                {8, 124, 0, 255},
+                {0, 118, 40, 255},
+                {0, 102, 120, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+                {236, 238, 236, 255},
+                {76, 154, 236, 255},
+                {120, 124, 236, 255},
+                {176, 98, 236, 255},
+                {228, 84, 236, 255},
+                {236, 88, 180, 255},
+                {236, 106, 100, 255},
+                {212, 136, 32, 255},
+                {160, 170, 0, 255},
+                {116, 196, 0, 255},
+                {76, 208, 32, 255},
+                {56, 204, 108, 255},
+                {56, 180, 204, 255},
+                {60, 60, 60, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+                {236, 238, 236, 255},
+                {168, 204, 236, 255},
+                {188, 188, 236, 255},
+                {212, 178, 236, 255},
+                {236, 174, 236, 255},
+                {236, 174, 212, 255},
+                {236, 180, 176, 255},
+                {228, 196, 144, 255},
+                {204, 210, 120, 255},
+                {180, 222, 120, 255},
+                {168, 226, 144, 255},
+                {152, 226, 180, 255},
+                {160, 214, 228, 255},
+                {160, 162, 160, 255},
+                {0, 0, 0, 255},
+                {0, 0, 0, 255},
+        };
+
+        void insertCartridge(std::shared_ptr<Cartridge> cartridge);
 
     private:
         uint8_t _tableName[2][1024]; // 2KB of name tables
-        uint8_t _tablePalette[32]; 
+        uint8_t _tablePalette[32];
 
         int16_t _scanline{0};
         int16_t _cycle{0};
 
         bool _frameComplete{false};
-        
+
+        std::shared_ptr<Cartridge> _cartridge{nullptr};
+
         SDL_Window *_window{nullptr};
         SDL_Renderer *_renderer{nullptr};
         bool _running{true};
 
-        Color _clearColor {0, 0, 0, 255};
+        SDL_Color _clearColor{0, 0, 0, 255};
+
+        enum class Status : uint8_t
+        {
+            V = 7, // Vertical blank
+            S = 6, // Sprite 0 Hit
+            O = 5, // Sprite Overflow
+        };
+
+        enum class Mask : uint8_t
+        {
+            G = 0,  // Greyscale
+            m = 1,  // Background left column enable
+            M = 2,  // Sprite left column enable
+            b = 3,  // Background enable
+            s = 4,  // Sprite enable
+            R = 5,  // Emphasize red
+            Gr = 6, // Emphasize green
+            B = 7,  // Emphasize blue
+        };
+
+        enum class Control : uint8_t
+        {
+            Nx = 0, // Nametable select / X scroll bit
+            Ny = 1, // Nametable select / Y scroll bit
+            I = 2,  // Increment mode
+            S = 3,  // Sprite tile select
+            B = 4,  // Background tile select
+            H = 5,  // Sprite height
+            P = 6,  // PPU master/slave select
+            V = 7,  // NMI enable
+        };
+
+        uint8_t _control{0};
+        uint8_t _mask{0};
+        uint8_t _status{0};
+        
+        uint8_t _addressLatch{0};
+        uint8_t _ppuDataBuffer{0};
+        uint16_t _vramAddress{0};
+
+        void setFlag(Control flag, bool value);
+        void setFlag(Mask flag, bool value);
+        void setFlag(Status flag, bool value);
+        uint8_t getFlag(Control flag) const;
+        uint8_t getFlag(Mask flag) const;
+        uint8_t getFlag(Status flag) const;
 
         void createWindow(const char *title, int width, int height);
         void destroyWindow() noexcept;
--- src/nes_ppu.cpp
+++ src/nes_ppu.cpp
@@ -1,9 +1,10 @@
 #include "nes_ppu.h"
 #include <stdexcept>
+#include "nes.h"
 
 using namespace NES;
 
-PPU::PPU(const char* title, int width, int height)
+PPU::PPU(const char *title, int width, int height)
 {
     if (!SDL_Init(SDL_INIT_VIDEO))
         throw std::runtime_error(SDL_GetError());
@@ -17,7 +18,7 @@ PPU::~PPU() noexcept
     SDL_Quit();
 }
 
-void PPU::createWindow(const char* title, int width, int height)
+void PPU::createWindow(const char *title, int width, int height)
 {
     if (!SDL_CreateWindowAndRenderer(title, width, height, 0, &_window, &_renderer))
     {
@@ -40,7 +41,7 @@ void PPU::destroyWindow() noexcept
     }
 }
 
-void PPU::handleEvents(std::function<void(SDL_Event&)> callback)
+void PPU::handleEvents(std::function<void(SDL_Event &)> callback)
 {
     SDL_Event event;
     while (SDL_PollEvent(&event))
@@ -63,13 +64,18 @@ void PPU::present()
     SDL_RenderPresent(_renderer);
 }
 
-void PPU::setClearColor(Color color)
+void PPU::setClearColor(SDL_Color color)
 {
     _clearColor = color;
 }
 
 void PPU::clock()
 {
+    if (_scanline == 241 && _cycle == 1)
+    {
+        setFlag(Status::V, true); // Set VBlank flag
+    }
+
     _cycle++;
 
     if (_cycle > 340)
@@ -82,4 +88,180 @@ void PPU::clock()
             _frameComplete = true;
         }
     }
+}
+
+void PPU::insertCartridge(std::shared_ptr<Cartridge> cartridge)
+{
+    _cartridge = cartridge;
+}
+
+uint8_t PPU::ppuRead(uint16_t address)
+{
+    uint8_t data = 0x00;
+    address &= 0x3FFF;
+
+    if (address <= 0x1FFF) // CHR-ROM
+    {
+        auto mapped = _cartridge->ppuRead(address);
+
+        if (mapped)
+            data = *mapped;
+    }
+    else if (address <= 0x3EFF) // nametables
+    {
+        // _tableName reads
+    }
+    else // $3F00-$3FFF palette
+    {
+        address &= 0x001F;
+        if (address == 0x0010)
+            address = 0x0000;
+        else if (address == 0x0014)
+            address = 0x0004;
+        else if (address == 0x0018)
+            address = 0x0008;
+        else if (address == 0x001C)
+            address = 0x000C;
+        data = _tablePalette[address];
+    }
+
+    return data;
+}
+
+void PPU::ppuWrite(uint16_t address, uint8_t value)
+{
+    address &= 0x3FFF;
+
+    if (address <= 0x1FFF) // CHR-ROM
+    {
+        _cartridge->ppuWrite(address, value);
+    }
+    else if (address <= 0x3EFF) // nametables
+    {
+        // _tableName writes
+    }
+    else // $3F00-$3FFF palette
+    {
+        address &= 0x001F;
+        if (address == 0x0010)
+            address = 0x0000;
+        else if (address == 0x0014)
+            address = 0x0004;
+        else if (address == 0x0018)
+            address = 0x0008;
+        else if (address == 0x001C)
+            address = 0x000C;
+        _tablePalette[address] = value;
+    }
+}
+
+void PPU::cpuWrite(uint16_t address, uint8_t value)
+{
+    switch (address)
+    {
+    case 0x0000: // Control
+        _control = value;
+        break;
+    case 0x0001: // Mask
+        _mask = value;
+        break;
+    case 0x0002: // Status
+        break;   // Read-only
+    case 0x0003: // OAM Address
+        break;   // Not implemented
+    case 0x0004: // OAM Data
+        break;   // Not implemented
+    case 0x0005: // Scroll
+        break;   // Not implemented
+    case 0x0006: // PPU Address
+        if (_addressLatch == 0)
+        {
+            _vramAddress = (_vramAddress & 0x00FF) | ((uint16_t)value << 8); // High byte
+            _addressLatch = 1;
+        }
+        else
+        {
+            // Second write (low byte)
+            _vramAddress = (_vramAddress & 0xFF00) | value; // Low byte
+            _addressLatch = 0;
+        }
+        break;
+    case 0x0007: // PPU Data
+        ppuWrite(_vramAddress, value);
+        _vramAddress++;  
+        break;
+    }
+}
+
+uint8_t PPU::cpuRead(uint16_t address)
+{
+    uint8_t data = 0x00;
+    switch (address)
+    {
+    case 0x0000: // Control
+        break;
+    case 0x0001: // Mask
+        break;
+    case 0x0002: // Status
+        data = (_status & 0xE0) | (_ppuDataBuffer & 0x1F); // Return status and lower bits of last read
+        setFlag(Status::V, false); // Clear VBlank flag
+        _addressLatch = 0; // Reset address latch
+        break;
+    case 0x0003: // OAM Address
+        break;   // Not implemented
+    case 0x0004: // OAM Data
+        break;   // Not implemented
+    case 0x0005: // Scroll
+        break;   // Not implemented
+    case 0x0006: // PPU Address
+        break;   // Not implemented
+    case 0x0007: // PPU Data
+        data = _ppuDataBuffer;
+        _ppuDataBuffer = ppuRead(_vramAddress);
+
+        if (_vramAddress >= 0x3F00)
+            data = _ppuDataBuffer; // No delay for palette reads
+
+        _vramAddress++;
+    }
+    return data;
+}
+
+void PPU::setFlag(Control flag, bool value)
+{
+    if (value)
+        NES::setBit(_control, static_cast<uint8_t>(flag));
+    else
+        NES::clearBit(_control, static_cast<uint8_t>(flag));
+}
+
+void PPU::setFlag(Mask flag, bool value)
+{
+    if (value)
+        NES::setBit(_mask, static_cast<uint8_t>(flag));
+    else
+        NES::clearBit(_mask, static_cast<uint8_t>(flag));
+}
+
+void PPU::setFlag(Status flag, bool value)
+{
+    if (value)
+        NES::setBit(_status, static_cast<uint8_t>(flag));
+    else
+        NES::clearBit(_status, static_cast<uint8_t>(flag));
+}
+
+uint8_t PPU::getFlag(Status flag) const
+{
+    return NES::getBit(_status, static_cast<uint8_t>(flag));
+}
+
+uint8_t PPU::getFlag(Control flag) const
+{
+    return NES::getBit(_control, static_cast<uint8_t>(flag));
+}
+
+uint8_t PPU::getFlag(Mask flag) const
+{
+    return NES::getBit(_mask, static_cast<uint8_t>(flag));
 }
